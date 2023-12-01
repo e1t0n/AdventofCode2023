3668f91660e0c78ae4e958ee61a2aded7e0ad8de
e1t0n <eltonlivera@gmail.com>
2023-12-01 13:34:48 +0000

    Day 01 Part 01

--- 1/adventofcode/main.swift
+++ 1/adventofcode/main.swift
@@ -1,4 +1,20 @@
 import Foundation
 
-let signals = sample.split(whereSeparator: \.isNewline)
-print(signals)
+let calibrationValues = input
+    .split(whereSeparator: \.isNewline)
+    .map { String($0) }
+
+func part1(_ values: [String]) {
+    let result = values
+        .map {
+            let value = $0.compactMap { Int(String($0)) != nil ? String($0) : nil }            
+            return value.first! + value.last!
+        }
+        .map { Int($0)! }
+        .reduce(0, +)
+            
+    print(result)
+}
+
+part1(calibrationValues) //55538
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# AdventofCode2023
+
+Source file path: [Day]/adventofcode/main.swift
