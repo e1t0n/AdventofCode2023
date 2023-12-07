f55c35230cceaa30406b47054db5235e03471289
Elton <elton.livera@natwest.com>
2023-12-07 11:12:06 +0000

    Day 05 Part 02

--- 05/adventofcode/main.swift
+++ 05/adventofcode/main.swift
@@ -1,34 +1,47 @@
 import Foundation
 import Algorithms
 
-let almanac = sample
+let almanac = input
     .split(whereSeparator: \.isNewline)
     .map(String.init)
 
 func part1() {
-    let ranges = getRanges(Array(almanac.dropFirst()))
     let seeds = almanac[0]
         .split(whereSeparator: \.isWhitespace)
         .map(String.init)
         .compactMap(Int.init)
-    var min = Int.max
-    for seed in seeds {
-        var _seed = seed
+    let ranges = getRanges(Array(almanac.dropFirst()))
+    print(getMinLocation(seeds: seeds, ranges))
+}
+
+//Brute force, not efficient
+func part2() {
+    let ranges = getRanges(Array(almanac.dropFirst()))
+    let seedRanges: [Range<Int>] = almanac[0]
+        .split(whereSeparator: \.isWhitespace)
+        .map(String.init)
+        .compactMap(Int.init)
+        .chunks(ofCount: 2)
+        .map(Array.init)
+        .map { ($0[0]..<$0[0]+$0[1]) }
+    
+    var minLoc = Int.max
+    for seedRange in seedRanges {
         for range in ranges {
-            for (s,d) in zip(range.s, range.d) {
-                if s.contains(_seed) {
-                    _seed = _seed - s.lowerBound + d.lowerBound
+            for s in range.s {
+                if seedRange.overlaps(s) {
+                    let result = getMinLocation(seeds: seedRange.clamped(to: s), ranges)
+                    minLoc = min(result, minLoc)
+                    print(result, "min:", minLoc)
                     break
                 }
             }
         }
-        if _seed < min { min = _seed }
     }
-    
-    print(min)
 }
 
 part1() //424490994
+part2() //15290096
 
 func getRanges(_ mappings: [String]) -> [(s:[Range<Int>], d:[Range<Int>])] {
     var maps = [(s:[Range<Int>], d:[Range<Int>])]()
@@ -52,4 +65,21 @@ func getRanges(_ mappings: [String]) -> [(s:[Range<Int>], d:[Range<Int>])] {
     }
     maps.append((sourceRange, destRange))
     return maps
+}
+
+func getMinLocation<T: Collection<Int>>(seeds: T, _ ranges: [(s:[Range<Int>], d:[Range<Int>])]) -> Int {
+    var min = Int.max
+    for seed in seeds {
+        var _seed = seed
+        for range in ranges {
+            for (s,d) in zip(range.s, range.d) {
+                if s.contains(_seed) {
+                    _seed = _seed - s.lowerBound + d.lowerBound
+                    break
+                }
+            }
+        }
+        if _seed < min { min = _seed }
+    }
+    return min
 }
